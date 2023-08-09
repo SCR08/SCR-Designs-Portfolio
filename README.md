752c41121eea28e672568d9afce00eacd0ab0da4
Sebastian Cruz <f1challenge2015@hotmail.com>
2023-08-09 16:24:22 -0500

    "first version"

---
+++ README.md
@@ -0,0 +1,1 @@
+SCR Designs Portfolio
---
+++ scrportfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css";
+import header from './components/header';
+
+const App = () => {
+  return (
+    <>
+    <header/>
+    </>
+  )
+
+}
+
+export default App
