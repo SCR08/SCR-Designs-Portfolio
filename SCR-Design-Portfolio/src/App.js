fb74c5f241614fad6a6105763bd9b5d2be63a948
Sebastian Cruz <f1challenge2015@hotmail.com>
2023-08-11 23:50:25 -0500

    home page in progress

--- SCR-Design-Portfolio/src/App.js
+++ SCR-Design-Portfolio/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import "./App.css";
 import Header from './components/header/Header';
+import Home from './components/Home/Home';
 
 const App = () => {
   return (
     <>
     <Header />
+    <main className='main'>
+      <Home />
+    </main>
     </>
   )
 }
---
+++ SCR-Design-Portfolio/src/components/Home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+   <div className="home__social">
+    <a href="https://www.linkedin.com/in/sebastian-cruz-r/" className="home__social-icon" target='_blank'>
+        <i class="uil uil-linkedin"></i>
+    </a>
+
+    <a href="https://github.com/SCR08" className="home__social-icon" target='_blank'>
+        <i class="uil uil-github"></i>
+    </a>
+
+    <a href="https://www.behance.net/sebastiancruz18" className="home__social-icon" target='_blank'>
+        <i class="uil uil-behance"></i>
+    </a>
+   </div>
+  )
+}
+
+export default Social
