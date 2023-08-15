55edbeb93739811773ba6b9919fb5c6766a03734
Sebastian Cruz <f1challenge2015@hotmail.com>
2023-08-14 20:28:14 -0500

    added soms CSS to skills. Flex not working

---
+++ SCR-Design-Portfolio/src/components/skills/UXDesign.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+
+const UXDesign = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title">Jr UX Designer</h3>
+
+        <div className="skills__box">
+            <div className="skills__group">
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+
+                    <div>
+                        <h3 className="skills__name">Empathize</h3>
+                        <span className="skills__level">Advanced</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+
+                    <div>
+                        <h3 className="skills__name">Research</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+
+                    <div>
+                        <h3 className="skills__name">Wireframing</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+
+                    <div>
+                        <h3 className="skills__name">Prototyping</h3>
+                        <span className="skills__level">Intermidiate</span>
+                    </div>
+
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+
+                    <div>
+                        <h3 className="skills__name">Testing</h3>
+                        <span className="skills__level">Intermidiate</span>
+                    </div>
+
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+
+                    <div>
+                        <h3 className="skills__name">Figma</h3>
+                        <span className="skills__level">Intermidiate</span>
+                    </div>
+
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+
+                    <div>
+                        <h3 className="skills__name">Adobe XD</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default UXDesign
