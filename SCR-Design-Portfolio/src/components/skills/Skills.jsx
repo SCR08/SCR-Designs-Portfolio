3366d5423c9cf019788ccb9f5c895436854b4473
Sebastian Cruz <f1challenge2015@hotmail.com>
2023-08-14 20:28:41 -0500

    Flex not working

--- SCR-Design-Portfolio/src/components/skills/Skills.jsx
+++ SCR-Design-Portfolio/src/components/skills/Skills.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './skills.css';
+import Frontend from './Frontend'
+import UXDesign from './UXDesign';
 
 const Skills = () => {
   return (
     <section className="skills section" id="skills">
         <h2 className="section__title">Skills</h2>
         <span className="section__subtitle">Skill level</span>
+
+        <div className="skills__container container grid">
+          <Frontend />
+          <UXDesign />
+        </div>
     </section>
   )
 }
