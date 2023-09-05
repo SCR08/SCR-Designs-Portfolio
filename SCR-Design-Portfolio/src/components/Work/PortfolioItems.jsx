61f89c5f97efe7eb38329f5f2aafb3735a3b39c6
Sebastian Cruz <f1challenge2015@hotmail.com>
2023-09-05 16:44:15 -0500

    Made channges to card components and fonts

--- SCR-Design-Portfolio/src/components/Work/PortfolioItems.jsx
+++ SCR-Design-Portfolio/src/components/Work/PortfolioItems.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const PortfolioItems = ({item}) => {
   return (
-    <div className="portfolio__card" key = {item.id}>
-        <img src={item.image} alt="" className='portfolio__img' />
-        <h3 className="portfolio__title">{item.title}</h3>
-        <a href={item.url} className="portfolio__button">
-            More Info <i className="bx bx-right-arrow-alt portfolio__button-icon"></i>
-        </a>
-    </div>
+    <a href={item.url} className="portfolio__card" target="_blank" rel="noopener noreferrer" key={item.id}>
+      <img src={item.image} alt="" className="portfolio__img" />
+      <h3 className="portfolio__title">{item.title}</h3>
+      <div className="portfolio__button">
+        More Info <i className="bx bx-right-arrow-alt portfolio__button-icon"></i>
+      </div>
+    </a>
   )
 }
 
